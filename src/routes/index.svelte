059b87550831a56eea0c3c9ad2eaeda4ab8a2d6a
dmiloloza <domagoj.miloloza@gmil.com>
2020-09-21 13:42:09 +0200

    Feat: footer

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,47 @@
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+    h1, p {
+        text-align: center;
+        margin: 0 auto;
+    }
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+    h1 {
+        font-size: 2em;
+        text-transform: uppercase;
+        font-weight: 700;
+        margin: 0 0 0.5em 0;
+    }
 
-	figure {
-		margin: 0 0 1em 0;
-	}
+    img {
+        width: 200px;
+        margin: 0 auto;
+        display: block;
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
+    }
 
-	p {
-		margin: 1em auto;
-	}
+    a {
+        margin-top: 40px;
+    }
 
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+    div{
+        margin-top: 2em;
+        text-align: center;
+    }
+
+    @media (min-width: 480px) {
+        h1 {
+            font-size: 4em;
+        }
+    }
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+    <title>Home page</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<div>
+    <img src="shield-letter-s-logo_8586-82.jpg" alt="image logo">
+    <h1>Simple page</h1>
+    <p>Find your next assignment right here.</p>
+    <a class="btn" href="jobs">View jobs</a>
+</div>
 
-<figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
