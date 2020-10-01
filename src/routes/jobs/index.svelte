f0b0c318a07b9558ec938b46d40924a995ff6f1d
dmiloloza <domagoj.miloloza@gmil.com>
2020-10-01 14:51:30 +0200

    Feat: footer,navbar

--- src/routes/jobs/index.svelte
+++ src/routes/jobs/index.svelte
@@ -1,1 +1,19 @@
-<h2>asdfa</h2>
+<script context="module">
+    export async function preload() {
+        const result = await this.fetch('/data.json');
+        const todos= await result.json();
+
+        return { todos }
+    }
+</script>
+
+<script>
+    export let todos;
+    console.log(todos);
+</script>
+
+<style>
+
+</style>
+
+<h2>All Current Jobs</h2>
