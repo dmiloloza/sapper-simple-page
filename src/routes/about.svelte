c87f3fc53f7b719efa1155061aa37f5d9baf3ee9
dmiloloza <domagoj.miloloza@gmil.com>
2020-10-09 09:04:44 +0200

    Feat: show/hide input type

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,8 +1,27 @@
 <svelte:head>
-	<title>About</title>
+    <title>About</title>
 </svelte:head>
+
+<script>
+    let inputEl;
+
+    let handleClick = () => {
+        console.log(inputEl.type);
+        inputEl.type = inputEl.type === 'password' ? 'text' : 'password';
+    }
+</script>
 
 <h1>About this site</h1>
 
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut dicta nulla porro sit tempore? Adipisci amet at aut corporis culpa eaque fugit inventore molestias optio tenetur! Accusamus aliquid dolorem tempora.</p>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut dicta nulla porro sit tempore? Adipisci amet at aut corporis culpa eaque fugit inventore molestias optio tenetur! Accusamus aliquid dolorem tempora.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut dicta nulla porro sit tempore? Adipisci amet at aut
+    corporis culpa eaque fugit inventore molestias optio tenetur! Accusamus aliquid dolorem tempora.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut dicta nulla porro sit tempore? Adipisci amet at aut
+    corporis culpa eaque fugit inventore molestias optio tenetur! Accusamus aliquid dolorem tempora.</p>
+
+
+<label>
+    <input bind:this={inputEl} type="password">
+</label>
+<button on:click={handleClick}>Toggle</button>
+
+<input type="text" placeholder="cool">
--- src/routes/jobs/create.svelte
+++ src/routes/jobs/create.svelte
@@ -6,13 +6,24 @@
     let details;
 
     let handleSubmit = async () => {
+        title = inputEl.value
+        console.log(inputEl.value)
         const res = await fetch('jobs.json', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({title, salary, details})
-        })
+        });
         const updatedJobs = await res.json();
         goto('jobs')
+    }
+
+    let emailLogin = false;
+    let inputEl;
+
+    let handleClick = () => {
+        console.log(inputEl.type, inputEl.value);
+        inputEl.type = inputEl.type === 'password' ? 'text' : 'password';
+
     }
 </script>
 
@@ -36,13 +47,39 @@
         border: 1px solid #ddd;
         border-radius: 6px;
     }
+    .tabs{
+        text-align: center;
+    }
+    .visibility {
+        border: none;
+        background: none;
+        cursor: pointer;
+        border-radius: 6px;
+        border: #ff3e00 solid 1px;
+        padding: 8px 18px;
+    }
 </style>
 
 
 <h2>Add a new Job</h2>
-<form on:submit|preventDefault={handleSubmit}>
-    <input bind:value={title} placeholder="job title" required type="text">
-    <input bind:value={salary} placeholder="salary" required type="number">
-    <textarea bind:value={details} placeholder="job details" required></textarea>
-    <button class="btn">Add new job</button>
-</form>
+<div class="tabs">
+    <button class="btn" class:active={emailLogin} on:click={() => emailLogin = true}>Email only</button>
+    <button class="btn" class:active={!emailLogin} on:click={() => emailLogin = false}>Email & password</button>
+</div>
+{#if emailLogin}
+    <form on:submit|preventDefault={handleSubmit}>
+        <input bind:value={title} placeholder="job title" required type="text">
+        <input bind:value={salary} placeholder="salary" required type="number">
+        <textarea bind:value={details} placeholder="job details" required></textarea>
+        <button class="btn">Add new job</button>
+    </form>
+{:else}
+    <form on:submit|preventDefault={handleSubmit}>
+        <input bind:this={inputEl} placeholder="TITLE TEST" required type="text">
+        <input bind:value={salary} placeholder="PLATA TEST" required type="number">
+        <textarea bind:value={details} placeholder="TEEEEEEEEEST job details" required></textarea>
+        <button type="button" class="visibility" on:click={handleClick}>Hide/Show</button>
+        <button class="btn">Add new job</button>
+    </form>
+{/if}
+
